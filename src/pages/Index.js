a3e94fdc378a05971e26b88202e653ec88bcb431
Kostja <k.gontscharow@gmail.com>
2016-03-09 21:29:35 +0100

    Displaying car parcs and availibility

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,10 +1,44 @@
 import React from 'react';
+import $ from 'jquery';
 
 export default class Index extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            parcButtons: []
+        };
+
+        this.parcButtons = null;
+        this.getParcs();
+    }
+
+    getParcs() {
+        var self = this;
+        $.ajax({
+            url: 'https://parkleit-api.codeformuenster.org/',
+            success: function (data) {
+                var parcs = JSON.parse(data).features;
+                var parcsButtons = [];
+                parcs.forEach((parcInfo) => {
+                    var parcProps = parcInfo.properties;
+                    parcsButtons.push(<p><button className="btn btn-default">{parcProps.name} ({parcProps.free}/{parcProps.total})</button></p>);
+                });
+
+                self.setState({
+                    parcButtons: parcsButtons
+                })
+            }
+        });
+    }
+
     render() {
         return (
-            <div className="jumbotron">
-                <h1>Finde Parkplätze</h1>
+            <div>
+                <div className="jumbotron">
+                    <h1>Finde Parkplätze</h1>
+                </div>
+                {this.state.parcButtons}
             </div>
         );
     }
